daaaf9d0d98c277b711601022d8269272cd6bf5c
Vijay Yevatkar <50692348+vijayyevatkar@users.noreply.github.com>
2021-05-13 23:37:09 +0530

    Added support to Windows 10

--- remindByDistrict.py
+++ remindByDistrict.py
@@ -1,27 +1,31 @@
 import webbrowser
 import requests
-import os
 import time
 import datetime
 import sys
-import subprocess
+from subprocess import check_call
+from plyer import notification
 
 headers_dict = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def sendOtp(mobile):
+# On windows:
+def windowsClip(txt):
+    cmd='echo '+txt.strip()+'|clip'
+    return check_call(cmd, shell=True)
+
+# On Mac
+def macbookClip(txt):
+    process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+    return process.communicate(mob.encode('utf-8'))
+
+# sendOtp(mobile, 1) => For Mac
+# sendOtp(mobile, 2) => For Windows
+def sendOtp(mobile, mode=1):
     mob = str(mobile)
-    # print("Sending OTP to Mobile: "+mob)
-    # postUrl = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
-    # res = requests.post(postUrl,json={'mobile':'{}'.format(mobile)})
-    # print("Response: "+res.text)
-    # if res.status_code == 200:
-    # print("OTP Sent successfully. Opening browser.")
     print("Opening browser.")
     webUrl = 'https://selfregistration.cowin.gov.in/'
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-    webbrowser.get(chrome_path).open(webUrl)
-    process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
-    temp = process.communicate(mob.encode('utf-8'))
+    webbrowser.open(webUrl,new=2)
+    temp = macbookClip(mob) if mode==1 else windowsClip(mob)
     print('Browser Opened, copied your number to clipboard. Just paste and hit Get OTP')
     print('Timing out for 200 secs') # Cowin waits for 180 secs after it sends OTP
     time.sleep(200)
@@ -30,24 +34,36 @@ path = "districts.csv"
 file2 = open(path,"r+")
 valid_district_ids = [int(i) for i in file2.read().split(',')]
 
-district_id = input('Enter district_id (Default is Dharwad: 278. Refer others in "districtNames.txt"): ')
-if district_id == '':
-    district_id = 278
-else:
-    district_id = int(district_id)
-assert district_id in valid_district_ids, "Enter a valid district id"
+goAhead = False
+trueList = ['','Y','y','Yes','yes','YEs','yEs','YeS','yeS','YES','yES']
+while not goAhead:
+    # Get the system mode 
+    system = input('Enter System Mode (Default is Macbook: 1), Enter 2 for Windows: ')
+    mode = 2 if system == '2' else 1
 
-num_days = input('Enter next \'n\' days that you want to check the slots (default is 5): ')
-if num_days == '':
-    num_days = 5
-else:
-    num_days = int(num_days)
-# print(num_days)
-assert num_days > 0 and num_days < 31, "Enter n > 0 or n < 31"
+    # Get the District ID
+    district_id = input('Enter district_id (Default is Dharwad: 278. Refer others in "districtNames.txt"): ')
+    if district_id == '':
+        district_id = 278
+    else:
+        district_id = int(district_id)
+    assert district_id in valid_district_ids, "Enter a valid district id"
 
-mob_num = input('Enter mobile number for clipboard copy: ')
-assert len(mob_num)==10, "Enter a valid mobile number"
-mob_num = int(mob_num)
+    # Get the next 'n' days for slot checks
+    num_days = input('Enter next \'n\' days that you want to check the slots (default is 5): ')
+    if num_days == '':
+        num_days = 5
+    else:
+        num_days = int(num_days)
+    assert num_days > 0 and num_days < 31, "Enter n > 0 or n < 31"
+
+    # Get the mobile number for copying it to clipboard
+    mob_num = input('Enter mobile number for clipboard copy: ')
+    print('\n===== Information Entered =====')
+    book = 'Macbook' if mode == 1 else 'Windows'
+    print('System: Windows\nDistrict: {0}\nCheck slot for days: {1}\nClipboard Mobile Number: {2}\n'.format(district_id,num_days,mob_num))
+    userConfirmation = input('Press enter to continue if above information is correct. No otherwise: ')
+    goAhead = True if userConfirmation in trueList else False
 
 k = 0
 print("\nStarting -- (Hit ^C to stop)\n===================================\n")
@@ -66,10 +82,11 @@ while True:
         print("District: "+str(district_id)+", Date: "+str(dateItem))
         response = requests.get(url,headers=headers_dict)
         answers = []
-        # print("Res len:" + str(len(responses))+ str(responses[-1].status_code))
+        # print("Res status:" + str(response.status_code))
         if response.status_code == 200:
             try:
                 items = response.json()['sessions']
+                # print(items)
                 for item in items:
                     if item['min_age_limit'] < 45:
                         center = str(item['name'])+", "+str(item['address'])+", "+str(item['date'])+", "+str(item['pincode'])
@@ -80,11 +97,14 @@ while True:
                         i=i+1
                         title = "New Slot-"+str(k)
                         message = answer
-                        command = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
-                        os.system(command)
+                        if mode == 1:                        
+                            command = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
+                            os.system(command)
+                        else:
+                            notification.notify(title= title,message= message,app_icon = None,timeout= 3600,toast=False)
                         sendOtp(mob_num)
             except:
-                print('Failed while parsing the response. Add below error to: '+"https://github.com/vijayyevatkar/macbook-cowin-reminder")
+                print('Failed while parsing the response. Create issue here: '+"https://github.com/vijayyevatkar/macbook-cowin-reminder/issues")
                 e = sys.exc_info()[0]
                 print(e)
         else:
